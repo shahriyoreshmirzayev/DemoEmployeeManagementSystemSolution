d7409b308fb470959f6c379b09faaad7d29bef4b
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2025-04-21 19:45:56 +0500

    Kamchilik bor html'da Register va Login qismida

---
+++ Client/ApplicationStates/DepartmentState.cs
@@ -0,0 +1,18 @@
+﻿namespace Client.ApplicationStates
+{
+    public class DepartmentState
+    {
+        public Action? GeneralDepartmentAction { get; set; }
+        public bool ShowGeneralDepartment { get; set; }
+        public void SetGeneralDepartmentClicked()
+        {
+            ResetAllDepartments();
+            ShowGeneralDepartment = true;
+            GeneralDepartmentAction?.Invoke();
+        }
+        private void ResetAllDepartments()
+        {
+            ShowGeneralDepartment = false;
+        }
+    }
+}
