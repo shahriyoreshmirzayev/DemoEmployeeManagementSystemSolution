56d60b3a9c62cd06a88a121b9d793280c2691c92
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2025-04-29 15:12:34 +0500

    Bootstrap'da kamchilik to'g'irlanmadi

--- Server/Program.cs
+++ Server/Program.cs
@@ -9,7 +9,6 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
